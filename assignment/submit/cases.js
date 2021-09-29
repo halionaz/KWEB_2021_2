f2afdb838b281b4b6ce11d62dd8b09bc5a59df7a
haSaJa <samhsj@naver.com>
2021-09-29 23:41:14 +0900

    작은 오류 수정

--- assignment/submit/cases.js
+++ assignment/submit/cases.js
@@ -11,11 +11,11 @@ const factorial = (N) => {
 
 const permutation = (n, r) => {
     return factorial(n)/factorial(n-r);
-}
+};
 
 const combination = (n, r) => {
     return factorial(n)/(factorial(n-r)*factorial(r));
-}
+};
 
 const dp = new Array();
 
@@ -38,11 +38,11 @@ const multiPermutation = (n, r) => {
         dp[r] = tmp**2;
     }
     return dp[r];
-}
+};
 
 const multiCombination = (n, r) => {
     return combination(n+r-1,r);
-}
+};
 
 module.exports = {
     permutation, combination, multiPermutation, multiCombination
