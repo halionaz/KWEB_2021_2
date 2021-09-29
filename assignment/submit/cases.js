7bd32a927ca0086464308422887f626568eb08c6
haSaJa <samhsj@naver.com>
2021-09-30 00:21:37 +0900

    가장 효율적 구현

--- assignment/submit/cases.js
+++ assignment/submit/cases.js
@@ -25,44 +25,6 @@ const multiCombination = (n, r) => {
     return combination(n+r-1,r);
 };
 
-// 분할정복
-
-// const dp = new Array();
-// const multiPermutation = (n, r) => {
-//     // base 처리
-//     if(dp[r]){
-//         return dp[r];
-//     }
-//     if(r == 0){
-//         return 1;
-//     }
-//     if(r == 1){
-//         return n;
-//     }
-//     if(r%2){
-//         dp[r] = multiPermutation(n,parseInt(r/2)+1) * multiPermutation(n,parseInt(r/2));
-//     } else {
-//         const tmp = multiPermutation(n,parseInt(r/2));
-//         dp[r] = tmp**2;
-//     }
-//     return dp[r];
-// };
-
-// 반복문
-
-// const multiPermutation = (n, r) => {
-//     if(r == 0) {
-//         return 1;
-//     } else {
-
-//         let ans = 1;
-//         for(let i = 1; i <= r; i++){
-//             ans *= n;
-//         }
-//         return ans;
-//     }
-// }
-
 module.exports = {
     permutation, combination, multiPermutation, multiCombination
 };
