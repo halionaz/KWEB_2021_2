8e7d57644f84b014952e20e64f2c2d9840c1f3a7
haSaJa <samhsj@naver.com>
2021-10-05 19:56:11 +0900

    pug로 view engine 설정

--- index.js
+++ index.js
@@ -1,13 +1,35 @@
-const http = require("http");
+// const http = require("http");
 
+// const port = 3000;
+
+// const server = http.createServer((req,res) => {
+//     res.statusCode = 200;
+//     res.setHeader("Content-Type","text/plain");
+//     res.end("Hello, World!\n");
+// });
+
+// server.listen(port, () => {
+//     console.log(`Server running at http://localhost:${port}/`);
+// });
+
+const express = require('express');
+const router = require('./router');
+const app = express();
 const port = 3000;
 
-const server = http.createServer((req,res) => {
-    res.statusCode = 200;
-    res.setHeader("Content-Type","text/plain");
-    res.end("Hello, World!\n");
+app.use((req, res, next) => {
+    const randomNumber = Math.floor(Math.random() * 10);
+    console.log(`Random Number: ${randomNumber}`);
+    if (randomNumber % 3) return next();
+    else return res.sendStatus(403);
+});
+app.use((req, res, next) => {
+    const { method, path } = req;
+    console.log(`${method} ${path}`);
+    return next();
 });
 
-server.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}/`);
-});
+// app.get('/', (req, res) => res.send('GET /'));
+// app.post('/', (req, res) => res.send('POST /'));
+app.use('/', router);
+app.listen(port, () => console.log(`Server listening on port ${port}!`));
--- server.js
+++ server.js
@@ -1,21 +1,14 @@
 const express = require('express');
-const router = require('./router');
 const app = express();
 const port = 3000;
 
-app.use((req, res, next) => {
-    const randomNumber = Math.floor(Math.random() * 10);
-    console.log(`Random Number: ${randomNumber}`);
-    if (randomNumber % 3) return next();
-    else return res.sendStatus(403);
-});
-app.use((req, res, next) => {
-    const { method, path } = req;
-    console.log(`${method} ${path}`);
-    return next();
-});
+app.set('views', `${__dirname}/views`);
+app.set('view engine', 'pug');
 
-// app.get('/', (req, res) => res.send('GET /'));
-// app.post('/', (req, res) => res.send('POST /'));
-app.use('/', router);
+app.get('/', (req, res) => {
+    res.render("home");
+});
+app.get('/page', (req, res) => {});
+app.get('/posts', (req, res) => {});
+
 app.listen(port, () => console.log(`Server listening on port ${port}!`));
