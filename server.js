7e909f7fcfd3cfae5ddbfeda155fd23b778169f4
haSaJa <samhsj@naver.com>
2021-10-05 20:16:31 +0900

    pug 조건 / 반복문 테스트

--- server.js
+++ server.js
@@ -8,7 +8,19 @@ app.set('view engine', 'pug');
 app.get('/', (req, res) => {
     res.render("home");
 });
-app.get('/page', (req, res) => {});
-app.get('/posts', (req, res) => {});
+app.get('/page/:page', (req, res) => {
+    const {page} = req.params;
+    res.render('board', { page });
+});
+app.get('/posts/:until', (req, res) => {
+    const { until } = req.params;
+    const untilParsed = parseInt(until, 10);
+    const posts = [];
+    if (!isNaN(untilParsed)) {
+        for (let i = 0; i < untilParsed; i++) {
+            posts.push(`Post ${i + 1}`);
+} }
+    res.render('post', { posts });
+});
 
 app.listen(port, () => console.log(`Server listening on port ${port}!`));
