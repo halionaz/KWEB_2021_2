a251e822d042c681430258a535c63bb5f73e5e2b
haSaJa <samhsj@naver.com>
2021-10-05 19:47:05 +0900

    express.js routing

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
-const express = require("express");
+const express = require('express');
+const router = require('./router');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send("Hello World!");
+app.use((req, res, next) => {
+    const randomNumber = Math.floor(Math.random() * 10);
+    console.log(`Random Number: ${randomNumber}`);
+    if (randomNumber % 3) return next();
+    else return res.sendStatus(403);
+});
+app.use((req, res, next) => {
+    const { method, path } = req;
+    console.log(`${method} ${path}`);
+    return next();
 });
 
-app.listen(port, () => {
-    console.log(`Server listening on port ${port}!`);
-})
+// app.get('/', (req, res) => res.send('GET /'));
+// app.post('/', (req, res) => res.send('POST /'));
+app.use('/', router);
+app.listen(port, () => console.log(`Server listening on port ${port}!`));
