be9e4122c067354367d495c2b54d767ea2df83ce
haSaJa <samhsj@naver.com>
2021-11-08 00:17:26 +0900

    exercise5.2 update

---
+++ assignment/assignment5/exercise5.2.sql
@@ -0,0 +1,16 @@
+-- 1
+select users.id, users.name, tickets.seat_number from tickets left join users on tickets.user = users.id WHERE tickets.train = 11 ORDER BY tickets.seat_number;
+
+-- 2
+
+
+-- 3
+
+
+-- 4
+
+
+-- 5
+
+
+-- 6
