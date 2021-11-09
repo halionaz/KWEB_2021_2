951f795df397ff102470d17e9f09b361d0a4503c
haSaJa <samhsj@naver.com>
2021-11-09 19:44:56 +0900

    generate password func

--- encryption.js
+++ encryption.js
@@ -2,15 +2,18 @@ const util = require('util');
 const crypto = require('crypto');
 
 const pbkdf2 = util.promisify(crypto.pbkdf2);
+const randomBytes = util.promisify(crypto.randomBytes);
 
-const encrypt = async text => {
+const geratePassword = async text => {
     const ALGO = 'sha512';
     const KEY_LEN = 64;
-    const digest = await pbkdf2(text, '', 1, KEY_LEN, ALGO);
-    console.log(`${text} | ${digest.toString('base64')}`);
+    const salt = await randomBytes(32);
+    const iter = Math.floor(Math.random() * 20000) + 200000;
+    const digest = await pbkdf2(text, salt, iter, KEY_LEN, ALGO);
+    return `${ALGO}:${salt.toString('base64')}:${iter}:${KEY_LEN}:${digest.toString('base64')}`;
 };
 
 (async () => {
-    await encrypt('samplepassword');
-    await encrypt('samplepasswort');
+    const hashed = await geratePassword('1234');
+    console.log(hashed);
 })();
